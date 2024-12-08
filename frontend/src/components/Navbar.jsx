b48f6d1d7cb09ccfa18ad6f970813639d534f37e
Ziya235 <144520360+Ziya235@users.noreply.github.com>
2024-12-08 23:00:51 +0400

    Update logo image

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -122,11 +122,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center p-3 px-4 md:px-16 bg-white shadow-md">
           {/* Logo */}
           <div className="flex items-center space-x-4">
-            <img
-              src="/api/placeholder/50/50"
-              alt="Logo"
-              className="h-10 w-10 rounded-full"
-            />
+           
             <h1 className="text-xl font-bold text-gray-800">School Platform</h1>
           </div>
 
